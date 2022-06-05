6cc4a8284bddcd45fcdd0f82568c814b3ea56540
lianna <673399718@qq.com>
2022-06-06 05:51:00 +0800

    update index.js

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # changmaoxiangCrawler
 长毛象爬虫
+
+yarn install (安装依赖)
+
+node index.js (启动命令)
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const axios = require('axios')
 const fs = require('fs');
 
-const firstid = '108423304618073096';
-const url = 'https://mastodon.online/api/v1/accounts/136306/statuses?exclude_replies=true&max_id='
+const firstid = '108414544572573503';
+const url = 'https://mastodon.online/api/v1/accounts/133400/statuses?exclude_replies=true&max_id='
 const reqPromise = url => axios.get(url);
 const dataLengthLimit = 400;// 到达上限存储json文件
+let DirectorFlag= false;
 let DataList = []
-
 function writeFile(data, fileName) {
     fs.writeFile(fileName, JSON.stringify(data), 'utf8', (err) => {
         if (err) throw err;
@@ -20,7 +20,13 @@ async function crawler(URL) {
     let lastItem = data[data.length-1]
     let id = lastItem.id;
     let fileName = `./${lastItem.account.username}/${lastItem.created_at.slice(0,10)+'-'+id}.json`;
-    
+    if(!DirectorFlag){
+        //文件夹不存在则创建data文件夹存储json文件 
+        if(!fs.existsSync(`./${lastItem.account.username}`)){
+            fs.mkdirSync(`./${lastItem.account.username}`)
+            DirectorFlag = true;        
+        }
+    }
     data.map((item)=>{
         DataList.push(item.content)
     })
